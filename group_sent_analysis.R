6a1efbb343d41750b4e55c70b02f342fa3a4b52a
LonelyRider-cs <54919991+LonelyRider-cs@users.noreply.github.com>
2020-10-13 14:23:50 -0600

    Update group_sent_analysis.R
    american and british in single file, functions A through G are setup, and some ggplots are left to create

--- group_sent_analysis.R
+++ group_sent_analysis.R
@@ -73,16 +73,98 @@ american20 <- american20[21:24362, ]
 american21 <- gutenberg_download(2046, meta_fields = c("title", "author"))
 american21 <- american21[140:5860, ]
 
-#bind all the novesl together
+#download all british novels individually
+brit1 <- gutenberg_download((768), meta_fields = c("title", "author")) #Wuthering Heights 
+brit1 <- brit1[7:12085, ]
+
+brit2 <- gutenberg_download((1260), meta_fields = c("title", "author")) #Jane Eyre 
+brit2 <- brit2[145:20659,]
+
+brit3 <- gutenberg_download((969), meta_fields = c("title", "author")) #The Tenant of Wildfell Hall
+brit3 <- brit3[433:18061,]
+
+brit4 <- gutenberg_download((11), meta_fields = c("title", "author")) #Alice's Adventures in Wonderland
+brit4 <- brit4[10:3339,]
+
+brit5 <- gutenberg_download((345), meta_fields = c("title", "author")) #Dracula 
+brit5 <- brit5[162:15482,]
+
+brit6 <- gutenberg_download((174), meta_fields = c("title", "author")) #The Picture of Dorian Gray
+brit6 <- brit6[58:8498,]
+
+brit7 <- gutenberg_download((766), meta_fields = c("title", "author")) #David Copperfield 
+brit7 <- brit7[161:38191,]
+
+brit8 <- gutenberg_download(36, meta_fields = c("title", "author"))  # the war of the worlds
+brit8 <- brit8[24:6474,]
+
+brit9 <- gutenberg_download(98, meta_fields = c("title", "author")) # a tale of two cities
+brit9 <- brit9[79:15865,]
+
+brit10 <- gutenberg_download(786, meta_fields = c("title", "author")) # hard times
+brit10 <- brit10[125:11629,]
+
+brit11 <- gutenberg_download(110, meta_fields = c("title", "author")) # tess of the d'Ubervilles
+brit11 <- brit11[46:17615,]
+
+brit12 <- gutenberg_download(145, meta_fields = c("title", "author")) #middlemarch
+brit12 <- brit12[81:33280,]
+
+brit13 <- gutenberg_download(219, meta_fields = c("title", "author")) #heart of darkness
+brit13 <- brit13[11:3312,]
+
+brit14 <- gutenberg_download(4276, meta_fields = c("title", "author")) #north and south
+brit14 <- brit14[31:19064,]
+
+brit15 <- gutenberg_download(21839, meta_fields = c("title", "author")) #sense and sensibility
+brit15 <- brit15[429:13242,]
+
+brit16 <- gutenberg_download(42671, meta_fields = c("title", "author")) #pride and prejudice
+brit16 <- brit16[59:13297,]
+
+brit17 <- gutenberg_download(105, meta_fields = c("title", "author")) #persuasion
+brit17 <- brit17[16:8324,]
+
+brit18 <- gutenberg_download(84, meta_fields = c("title", "author")) #frankenstein
+brit18 <- brit18[16:7244,]
+
+brit19 <- gutenberg_download(82, meta_fields = c("title", "author")) #ivanhoe: a romance
+brit19 <- brit19[811:19722,]
+
+brit20 <- gutenberg_download(7412, meta_fields = c("title", "author")) #coningsby, or the new generation
+brit20 <- brit20[169:17567,]
+
+brit21 <- gutenberg_download(3760, meta_fields = c("title", "author")) # sybil or, the two nations
+brit21 <- brit21[43:17875,]
+
+brit22 <- gutenberg_download(599, meta_fields = c("title", "author"))   # vanity fair
+brit22 <- brit22[13:30002,]
+
+brit23 <- gutenberg_download(2153, meta_fields = c("title", "author"))   # mary barton
+brit23 <- brit23[52:17825,]
+
+#bind all the american novels together
 american_corpus <- rbind(american1, american2, american3, american4, american5, american6, american7, american8, american9, american10, american11, american12, american13, american14, american15, american16, american17, american18, american19, american20, american21)
 #unnest all words
 american_corpus_tokens <- american_corpus %>% unnest_tokens(word, text)
-
 #remove stop words from corpus
 american_corpus_tokens_stop_words_removed <- american_corpus_tokens %>% anti_join(stop_words)
 
+#bind all the british novels together
+british_corpus <- rbind(brit1, brit2, brit3, brit4, brit5, brit6, brit7, brit8, brit9, brit10, brit11, brit12, brit13, brit14, brit15, brit16, brit17, brit18, brit19, brit20, brit21, brit22, brit23)
+#unests all words
+british_corpus_tokens <- british_corpus %>% unnest_tokens(word, text)
+#remove all stop words
+british_corpus_tokens_stop_words_removed <- british_corpus_tokens %>% anti_join(stop_words)
+
+
 # A: the top 20 frequently used words in the american corpus
 american_wc_20 <- american_corpus_tokens_stop_words_removed %>% 
+  count(word, sort = TRUE) %>% 
+  top_n(20) %>% 
+  mutate(word = reorder(word, n))
+
+british_wc_20 <- british_corpus_tokens_stop_words_removed %>% 
   count(word, sort = TRUE) %>% 
   top_n(20) %>% 
   mutate(word = reorder(word, n))
@@ -92,6 +174,7 @@ american_wc_20 <- american_corpus_tokens_stop_words_removed %>%
 #by calculating theratio of negativesentiment words to total emotion words
 BING <- get_sentiments("bing")
 
+#american
 american_sentiment_ratio <- american_corpus_tokens %>% 
   group_by(title) %>% 
   inner_join(BING) %>% 
@@ -117,8 +200,34 @@ ggplot(american_sentiment_ratio, aes(title, positive_ratio)) +
        caption="source: Project Gutenberg") +  
   coord_flip()
   
+#british
+british_sentiment_ratio <- british_corpus_tokens %>% 
+  group_by(title) %>% 
+  inner_join(BING) %>% 
+  count(sentiment) %>% 
+  spread(sentiment, n) %>% 
+  mutate(sentiment = positive - negative) %>% 
+  mutate(positive_ratio = (positive)/(positive + negative)) %>% 
+  ungroup() %>% 
+  mutate(title = reorder(title, positive_ratio)) %>% 
+  top_n(-10)
+
+
+ggplot(british_sentiment_ratio, aes(title, positive_ratio)) +
+  geom_point(col="tomato2", size=3) +   # Draw points, specifying point color and size
+  geom_segment(aes(x=title, 
+                   xend=title, 
+                   y=min(positive_ratio), 
+                   yend=max(positive_ratio)), 
+               linetype="dashed", 
+               size=0.1) +              # Draw dashed lines
+  labs(title="Ratio of Positive Words to Total Emotions Words (Bing Lexicon)", 
+       subtitle="Major British Novels Corpus", 
+       caption="source: Project Gutenberg") +  
+  coord_flip()
 
 #C: Using the same sentiment lexicon, determine the overall sentiment of each corpora.
+#american
 american_sentiment_corpus <- american_corpus_tokens %>% 
   inner_join(BING) %>% 
   count(sentiment) %>% 
@@ -126,9 +235,17 @@ american_sentiment_corpus <- american_corpus_tokens %>%
   mutate(sentiment = positive - negative) %>% 
   mutate(positive_ratio = (positive)/(positive + negative))
 
+#british
+british_sentiment_corpus <- british_corpus_tokens %>% 
+  inner_join(BING) %>% 
+  count(sentiment) %>% 
+  spread(sentiment, n) %>% 
+  mutate(sentiment = positive - negative) %>% 
+  mutate(positive_ratio = (positive)/(positive + negative))
 #still need plot compairng british and american sentitment
   
 #D: What fifteen words contribute most to the positive and negative scores of eachcorpus
+#american
 american_top_15_sentiments <- american_corpus_tokens %>%
   inner_join(BING) %>%
   count(word, sort = TRUE) %>%
@@ -142,16 +259,85 @@ ggplot(american_top_15_sentiments, aes(word, n)) +
        subtitle = "BING sentiment lexicon")+
   coord_flip()
 
+#british
+british_top_15_sentiments <- british_corpus_tokens %>%
+  inner_join(BING) %>%
+  count(word, sort = TRUE) %>%
+  top_n(15) %>%
+  mutate(word = reorder(word, n))
 
 
+ggplot(british_top_15_sentiments, aes(word, n)) +
+  geom_col()+
+  labs(title = "Top BING Words in Major American Novels Corpus",
+       subtitle = "BING sentiment lexicon")+
+  coord_flip()
 
 
+#E: Using the NRC sentiment lexicon, find the top fifteen “anger” and “joy” words of 
+#each corpus.
+NRC <- lexicon_nrc()
+nrcJoy <- NRC %>% 
+  filter(sentiment == "joy")
+nrcAnger <- NRC %>% 
+  filter(sentiment == "anger")
 
+#american
+american_Joy <- american_corpus_tokens_stop_words_removed %>% 
+  inner_join(nrcJoy) %>% 
+  count(word, sort = TRUE) %>% 
+  top_n(15)
+american_Anger <- american_corpus_tokens_stop_words_removed %>% 
+  inner_join(nrcAnger) %>% 
+  count(word, sort = TRUE) %>% 
+  top_n(15)
 
+#british
+british_Joy <- british_corpus_tokens_stop_words_removed %>% 
+  inner_join(nrcJoy) %>% 
+  count(word, sort = TRUE) %>% 
+  top_n(15)
+british_Anger <- british_corpus_tokens_stop_words_removed %>% 
+  inner_join(nrcAnger) %>% 
+  count(word, sort = TRUE) %>% 
+  top_n(15)
 
+#F: Which novel in each corpus is characterized by the greatest level of disgust?
+nrcDisgust <- NRC %>% 
+  filter(sentiment == "disgust")
 
+#american
+american_DisgustNovels <- american_corpus_tokens_stop_words_removed %>% 
+  group_by(title) %>% 
+  inner_join(nrcDisgust) %>% 
+  count(sentiment) %>% 
+  spread(sentiment, n)
 
+#british
+british_DisgustNovels <- american_corpus_tokens_stop_words_removed %>% 
+  group_by(title) %>% 
+  inner_join(nrcDisgust) %>% 
+  count(sentiment) %>% 
+  spread(sentiment, n)
 
+#G: Using the AFINN sentiment lexicon, find the five most positive and negative novels 
+#in your combined corpora.(AFINN)
+afinn <- lexicon_afinn()
+modAfinn <- afinn %>% 
+  filter(word != "miss")
+#american
+american_Afinn <- american_corpus_tokens_stop_words_removed %>% 
+  group_by(title) %>% 
+  inner_join(modAfinn) %>% 
+  summarise(sentiment = sum(value)) %>% 
+  ungroup() %>% 
+  mutate(title = reorder(title, sentiment))
 
-
+#british
+british_Afinn <- british_corpus_tokens_stop_words_removed %>% 
+  group_by(title) %>% 
+  inner_join(modAfinn) %>% 
+  summarise(sentiment = sum(value)) %>% 
+  ungroup() %>% 
+  mutate(title = reorder(title, sentiment))
 
